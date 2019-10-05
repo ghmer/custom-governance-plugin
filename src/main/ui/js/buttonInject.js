cedeb81cd9837754219b7687d000042041f23036
Mario Ragucci <mario.ragucci@sailpoint.com>
2019-10-05 05:15:29 +0200

    cleanup
    Signed-off-by: Mario Ragucci <mario.ragucci@sailpoint.com>

--- src/main/ui/js/buttonInject.js
+++ src/main/ui/js/buttonInject.js
@@ -2,7 +2,9 @@ jQuery(document).ready(
 function() {
   var customGovernancePluginUrl = SailPoint.CONTEXT_PATH  + '/plugins/pluginPage.jsf?pn=custom_governance_plugin';
   jQuery("ul.navbar-right li:first").before(
-    '<li class="dropdown"><a href="'+ customGovernancePluginUrl + '" tabindex="0" role="menuitem" title="Custom Governance Plugin">'
-  + '<i role="presenation" class="fa fa-thumbs-o-up fa-lg example"></i>'
-  + '</a></li>');
+    '<li class="dropdown">'
+  +   '<a href="'+ customGovernancePluginUrl + '" tabindex="0" role="menuitem" title="Custom Governance Plugin">'
+  +     '<i role="presenation" class="fa fa-thumbs-o-up fa-lg example"></i>'
+  +   '</a>'
+  + '</li>');
 });
